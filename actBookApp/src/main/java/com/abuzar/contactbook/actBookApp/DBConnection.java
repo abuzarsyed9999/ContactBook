76a88be7cdfc26eae2a1200a5e771df9f61cc32b
abuzarsyed9999 <abuzarcda@gmail.com>
2026-02-02 14:16:58 +0530

    Update DBConnection.java

--- actBookApp/src/main/java/com/abuzar/contactbook/actBookApp/DBConnection.java
+++ actBookApp/src/main/java/com/abuzar/contactbook/actBookApp/DBConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class DBConnection {
 	 private static final String URL = "jdbc:mysql://localhost:3306/contact_book_db";
 	    private static final String USERNAME = "root";         
-	    private static final String PASSWORD = "#Abuzar99";
+	    private static final String PASSWORD = "password";
 	    
 	    static 
 	    {
